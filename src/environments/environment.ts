74c34d333a7ca9ce0be1bfd4122ca946cc65778e
abel serra <aserra@grupov.com.ve>
2026-05-12 13:39:23 -0400

    Add new logo images and update logo references in login and expenses pages
    This commit introduces three new logo images: 'logo-completo.png', 'logo-remove.png', and 'titulo-remove.png'. It updates the login page to display 'logo-completo.png' prominently and modifies the expenses page to include 'logo-remove.png' for desktop and 'titulo-remove.png' for mobile views. Additionally, the app's origin URL is adjusted in the environment configuration for consistency.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@ export const environment = {
   production: false,
   apikey: 'OpPvE+U+HA+2e0MURoF+9Z0RoJLTg4/ofvFOgDY9CpA=',
   apiUrl: 'http://localhost:3088/api',
-  appOriginUrl: 'http://localhost:4300',
+  appOriginUrl: 'http://localhost:4200',
   firebase: firebaseWebConfig,
 };
